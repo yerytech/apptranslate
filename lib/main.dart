c3601cf46bd01c625c2e9cd0c778473ba1ee04a0
yerytech <202000854@p.uapa.edu.do>
2024-04-28 07:51:12 -0700

    Add portraitUp Orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'pages/pages.dart';
 
@@ -6,10 +7,16 @@ import 'pages/pages.dart';
 void main() => runApp(const ProviderScope(child: MyApp()));
 
 class MyApp extends StatelessWidget {
+  
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp
+    ]);
+
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Translate',
